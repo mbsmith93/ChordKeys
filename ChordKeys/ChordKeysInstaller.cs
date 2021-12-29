6735524982a10b7c8e6f3eb9ffa4aecb94ba7940
mbsmith93 <mbsmith93@gmail.com>
2021-12-29 00:42:00 -0600

    Add better installation project.

---
+++ ChordKeys/ChordKeysInstaller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace ChordKeys
+{
+    [RunInstaller(true)]
+    public partial class ChordKeysInstaller : System.Configuration.Install.Installer
+    {
+        public ChordKeysInstaller()
+        {
+            InitializeComponent();
+            this.Committed += new InstallEventHandler(MyInstaller_Committed);
+        }
+        private void MyInstaller_Committed(object sender, InstallEventArgs e)
+        {
+            Directory.SetCurrentDirectory(Path.GetDirectoryName
+            (Assembly.GetExecutingAssembly().Location));
+            Process.Start(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\ChordKeys.exe");
+        }
+    }
+}
